d760e1ade574b155b191a1c5f1c74fc7fd55edd8
OfftheHeZie <jerryfeltz@server.fake>
2013-02-15 18:58:08 -0600

    Adding files with VS2010 project folder

---
+++ testRepo/common.h
@@ -0,0 +1,27 @@
+#ifndef INC_COMMON_H
+#define INC_COMMON_H
+
+#if		_MSC_VER > 1000
+#pragma once
+
+#ifdef WIN32
+#include <WinSock2.h>
+#include <io.h>
+#pragma comment(lib,"Ws2_32.lib")
+#endif
+
+#ifdef LINUX
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <arpa/inter.h>
+#endif
+
+#include <time.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#endif
+#endif
